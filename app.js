3b3295c456fc91023b1d0425aa957ad0737d6629
Julien Philippot <50954315+JulienPhlpt@users.noreply.github.com>
2020-10-28 17:22:25 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
+// Responsive navbar
 const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-  burger.addEventListener('click', () => {
-  // toggle navigation bar
-    nav.classList.toggle('nav-acvtive');
-
+  burger.addEventListener("click", () => {
+    // toggle navigation bar
+    nav.classList.toggle("nav-acvtive");
 
     // Links animation
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
-        link.style.animation = '';
+        link.style.animation = "";
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
       }
     });
     // Burger animation
-    burger.classList.toggle('toggle');
+    burger.classList.toggle("toggle");
   });
-}
+};
 
 navSlide();
