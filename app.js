b331f08e1361eef27f41929e36c6e08a51fb397b
Julien Philippot <50954315+JulienPhlpt@users.noreply.github.com>
2020-10-28 17:43:19 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -23,4 +23,48 @@ const navSlide = () => {
   });
 };
 
+const gradient = () => {
+  const sections = document.querySelectorAll("section");
+  const bubble = document.querySelector(".bubble");
+  const gradients = [
+    "linear-gradient(to left, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%)",
+    "linear-gradient(to right, #ffecd2 0%, #fcb69f 100%)",
+    "linear-gradient(to left, #fad0c4 0%, #ffd1ff 100%)",
+    "linear-gradient(to right, #fdcbf1 0%, #fdcbf1 1%, #e6dee9 100%)",
+  ];
+
+  const options = {
+    threshold: 0.7,
+  };
+
+  let observer = new IntersectionObserver(navCheck, options);
+
+  function navCheck(entries) {
+    entries.forEach((entry) => {
+      const className = entry.target.className;
+      const activeAnchor = document.querySelector(`[data-page=${className}]`);
+      const gradientIndex = entry.target.getAttribute("data-index");
+      const coords = activeAnchor.getBoundingClientRect();
+      const directions = {
+        height: coords.height,
+        width: coords.width,
+        top: coords.top,
+        left: coords.left,
+      };
+      if (entry.isIntersecting) {
+        bubble.style.setProperty("left", `${directions.left}px`);
+        bubble.style.setProperty("top", `${directions.top}px`);
+        bubble.style.setProperty("width", `${directions.width}px`);
+        bubble.style.setProperty("height", `${directions.height}px`);
+        bubble.style.background = gradients[gradientIndex];
+      }
+    });
+  }
+
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+};
+
 navSlide();
+gradient();
